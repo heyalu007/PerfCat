21063961c1ac0576bc6ce8fc78a3b16bc21dbf0f
lucas <lucasylhe@tencent.com>
2021-12-24 17:48:49 +0800

    添加了简单的UI

--- PerfCat_MacOS_Windows/main.py
+++ PerfCat_MacOS_Windows/main.py
@@ -4,10 +4,14 @@
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
+
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QTextBrowser
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
 
 # from tidevice.__main__ import *
 from tidevice._usbmux import *
+
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -18,21 +22,42 @@ def print_hi(name):
     print(list)
 
 
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        layout = QVBoxLayout()
+        device_list_button = QPushButton()
+        device_list_button.setText('获取设备列表')
+        device_list_button.move(100, 100)
+        device_list_button.resize(50, 60)
+        device_list_button.clicked.connect(self.clickAction)
+        layout.addWidget(device_list_button)
+
+        self.text_browser = QTextBrowser()
+        self.text_browser.resize(50, 400)
+        layout.addWidget(self.text_browser)
+
+        self.setLayout(layout)
+
+    def clickAction(self):
+        print("我被点击了")
+
+        um = Usbmux()
+        list = um.device_list()
+
+        self.text_browser.setText(str(list))
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
 
     app = QApplication(sys.argv)
-    w = QWidget()
+    w = MainWindow()
     w.resize(1000, 600)
     w.move(300, 300)
     w.setWindowTitle('我是性能猫')
     w.show()
-
-    device_list_button = QPushButton()
-    device_list_button.setText('获取设备列表')
-    device_list_button.move(100, 100)
-    # device_list_button.clicked().connect(test)
 
     # 进入程序的主循环
     sys.exit(app.exec_())
